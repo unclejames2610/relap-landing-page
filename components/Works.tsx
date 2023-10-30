b865cf01e58db906491f0c22030f7a7e1adc2287
unclejames2610 <udealaebuka@gmail.com>
2023-10-30 13:42:58 +0100

    feat:animated the works page

--- components/Works.tsx
+++ components/Works.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import FadeIn from "./FadeIn";
 import WorksGrid from "./WorksGrid";
 
 const Works: FC = () => {
@@ -9,13 +10,18 @@ const Works: FC = () => {
     >
       {/* writing */}
       <div className="flex items-center flex-col gap-5">
-        <h2 className="font-bold font-poppins lg:text-[56px] lg:leading-[64px] text-fontBlack text-[40px] leading-[48px]">
-          Fugiat quo
-        </h2>
-        <h5 className="font-semibold text-fontBlcak text-center md:w-[570px]">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam.
-        </h5>
+        <FadeIn delay={0.4} direction="down">
+          <h2 className="font-bold font-poppins lg:text-[56px] lg:leading-[64px] text-fontBlack text-[40px] leading-[48px]">
+            Fugiat quo
+          </h2>
+        </FadeIn>
+
+        <FadeIn delay={0.2} direction="down">
+          <h5 className="font-semibold text-fontBlcak text-center md:w-[570px]">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam.
+          </h5>
+        </FadeIn>
       </div>
 
       {/* cards */}
--- components/WorksGrid.tsx
+++ components/WorksGrid.tsx
@@ -6,71 +6,86 @@ import image4 from "@/public/assets/worksImage4.svg";
 import image5 from "@/public/assets/worksImage5.svg";
 import image6 from "@/public/assets/worksImage6.svg";
 import Image from "next/image";
+import FadeIn from "./FadeIn";
 
 const WorksGrid: FC = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      <div className="flex flex-col items-center p-4 py-12 cursor-pointer hover:shadow-xl">
-        <Image src={image1} alt="image1" />
-        <h4 className="font-bold font-poppins text-2xl text-fontBlack">
-          Consequuntur
-        </h4>
-        <p className="text-fontBlack/50 text-center md:w-[322px]">
-          Enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit
-        </p>
-      </div>
+      <FadeIn delay={0.6} direction="down">
+        <div className="flex flex-col items-center p-4 py-12 cursor-pointer hover:shadow-xl">
+          <Image src={image1} alt="image1" />
+          <h4 className="font-bold font-poppins text-2xl text-fontBlack">
+            Consequuntur
+          </h4>
+          <p className="text-fontBlack/50 text-center md:w-[322px]">
+            Enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+            fugit
+          </p>
+        </div>
+      </FadeIn>
 
-      <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
-        <Image src={image2} alt="image2" />
-        <h4 className="font-bold font-poppins text-2xl text-fontBlack">
-          Voluptatem
-        </h4>
-        <p className="text-fontBlack/50 text-center md:w-[322px]">
-          Sed quia non numquam eius modi tempora incidunt ut labore et dolore
-          magnam
-        </p>
-      </div>
+      <FadeIn delay={0.6} direction="down">
+        <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
+          <Image src={image2} alt="image2" />
+          <h4 className="font-bold font-poppins text-2xl text-fontBlack">
+            Voluptatem
+          </h4>
+          <p className="text-fontBlack/50 text-center md:w-[322px]">
+            Sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam
+          </p>
+        </div>
+      </FadeIn>
 
-      <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
-        <Image src={image3} alt="image3" />
-        <h4 className="font-bold font-poppins text-2xl text-fontBlack">
-          Quisquam
-        </h4>
-        <p className="text-fontBlack/50 text-center md:w-[322px]">
-          Quis nostrum exercitationem ullam corporis suscipit laboriosam
-        </p>
-      </div>
+      <FadeIn delay={0.6} direction="down">
+        <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
+          <Image src={image3} alt="image3" />
+          <h4 className="font-bold font-poppins text-2xl text-fontBlack">
+            Quisquam
+          </h4>
+          <p className="text-fontBlack/50 text-center md:w-[322px]">
+            Quis nostrum exercitationem ullam corporis suscipit laboriosam
+          </p>
+        </div>
+      </FadeIn>
 
-      <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
-        <Image src={image4} alt="image4" />
-        <h4 className="font-bold font-poppins text-2xl text-fontBlack">
-          Aliquid
-        </h4>
-        <p className="text-fontBlack/50 text-center md:w-[322px]">
-          Quis autem vel eum iure reprehenderit voluptate velit esse quam nihil
-        </p>
-      </div>
+      <FadeIn delay={0.8} direction="up">
+        <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
+          <Image src={image4} alt="image4" />
+          <h4 className="font-bold font-poppins text-2xl text-fontBlack">
+            Aliquid
+          </h4>
+          <p className="text-fontBlack/50 text-center md:w-[322px]">
+            Quis autem vel eum iure reprehenderit voluptate velit esse quam
+            nihil
+          </p>
+        </div>
+      </FadeIn>
 
-      <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
-        <Image src={image5} alt="image5" />
-        <h4 className="font-bold font-poppins text-2xl text-fontBlack">
-          Excepteur
-        </h4>
-        <p className="text-fontBlack/50 text-center md:w-[322px]">
-          Sint occaecat cupidatat non proident culpa qui officia deserunt mollit
-          anim
-        </p>
-      </div>
+      <FadeIn delay={0.8} direction="up">
+        <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
+          <Image src={image5} alt="image5" />
+          <h4 className="font-bold font-poppins text-2xl text-fontBlack">
+            Excepteur
+          </h4>
+          <p className="text-fontBlack/50 text-center md:w-[322px]">
+            Sint occaecat cupidatat non proident culpa qui officia deserunt
+            mollit anim
+          </p>
+        </div>
+      </FadeIn>
 
-      <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
-        <Image src={image6} alt="image6" />
-        <h4 className="font-bold font-poppins text-2xl text-fontBlack">
-          Accusamus
-        </h4>
-        <p className="text-fontBlack/50 text-center md:w-[322px]">
-          Odio dignissimos ducimus qui blanditiis praesentium voluptatum
-        </p>
-      </div>
+      <FadeIn delay={0.8} direction="up">
+        <div className="flex flex-col items-center cursor-pointer hover:shadow-xl p-4 py-12">
+          <Image src={image6} alt="image6" />
+          <h4 className="font-bold font-poppins text-2xl text-fontBlack">
+            Accusamus
+          </h4>
+          <p className="text-fontBlack/50 text-center md:w-[322px]">
+            Odio dignissimos ducimus qui blanditiis praesentium voluptatum
+          </p>
+        </div>
+      </FadeIn>
     </div>
   );
 };
