48ce8a22ff30fbdb48e2076fdd80917b6cccc922
unclejames2610 <udealaebuka@gmail.com>
2023-10-30 12:25:25 +0100

    feat:added animations to home and hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,38 +9,59 @@ import oval from "@/public/assets/Oval.svg";
 import Image from "next/image";
 import { Link } from "react-scroll";
 import { motion } from "framer-motion";
+import FadeIn from "./FadeIn";
+
+interface ListItemsProps {
+  id: number;
+  name: string;
+  url: string;
+}
 
 const Hero: FC = () => {
   const [navOpen, setNavOpen] = useState<boolean>(false);
+
+  const listItems: ListItemsProps[] = [
+    { id: 1, name: "Home", url: "homepage" },
+    { id: 2, name: "Features", url: "features" },
+    { id: 3, name: "Works", url: "works" },
+    { id: 4, name: "About", url: "about" },
+    { id: 5, name: "People", url: "people" },
+    { id: 2, name: "News", url: "news" },
+  ];
 
   const menuVariant = {
     hidden: {
       x: 100,
       y: -100,
       opacity: 0,
+      transition: {
+        when: "afterChildren",
+      },
     },
     visible: {
       x: 0,
       y: 0,
       opacity: 1,
-      borderBottomLeftRadius: ["100%", "0%"],
+      borderBottomLeftRadius: ["100%", "70%", "50%", "20%", "10%"],
+      borderBottomRightRadius: "10%",
 
       transition: {
         duration: 0.8,
         when: "beforeChildren",
+        delay: 0.5,
+        staggerChildren: 0.1,
       },
     },
   };
 
   const listVariant = {
     hidden: {
-      x: 100,
+      y: -20,
       opacity: 0,
     },
     visible: {
-      x: 0,
+      y: 0,
       opacity: 1,
-      staggerChildren: 0.2,
     },
   };
 
@@ -49,12 +70,29 @@ const Hero: FC = () => {
       <Navbar navOpen={navOpen} setNavOpen={setNavOpen} />
       {navOpen && (
         <motion.div
-          className="lg:hidden flex flex-col gap-6 font-semibold text-secondary items-center w-full absolute bg-white p-4 top-16 z-[100]"
+          className="lg:hidden font-semibold text-secondary text-center w-full absolute bg-white p-4 top-16 z-[100]"
           variants={menuVariant}
           animate="visible"
           initial="hidden"
         >
-          <motion.div variants={listVariant}>
+          {listItems.map((list) => (
+            <motion.li
+              key={list.id}
+              variants={listVariant}
+              className="cursor-pointer hover:text-[#FFC300] transition-all duration-500 pb-4 list-none"
+            >
+              <Link
+                to={list.url}
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={1000}
+              >
+                {list.name}
+              </Link>
+            </motion.li>
+          ))}
+          {/* <motion.div variants={listVariant}>
             <Link
               to="homepage"
               spy={true}
@@ -125,48 +163,55 @@ const Hero: FC = () => {
             >
               News
             </Link>
-          </motion.div>
+          </motion.div> */}
 
-          <Link
-            to="contact-us"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={1000}
+          <motion.button
+            className="border bg-secondary text-white p-2 px-10 border-secondary rounded-full font-bold hover:bg-transparent hover:text-secondary transition-all duration-500"
+            variants={listVariant}
           >
-            <motion.button
-              className="border bg-secondary text-white p-2 px-10 border-secondary rounded-full font-bold hover:bg-transparent hover:text-secondary transition-all duration-500"
-              variants={listVariant}
+            <Link
+              to="contact-us"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={1000}
             >
               Contact Us
-            </motion.button>
-          </Link>
+            </Link>
+          </motion.button>
         </motion.div>
       )}
       <div className="flex flex-col lg:flex-row lg:items-center justify-between p-2 lg:pt-16 lg:px-12 xl:px-36">
         {/* writing */}
         <div className="flex flex-col items-start p-2 gap-5 lg:gap-8">
-          <h1 className="font-poppins font-bold text-[56px] leading-[64px] lg:text-[72px] lg:leading-[72px] lg:w-[335px] w-[261px] text-white">
-            Omnis Voluptas
-          </h1>
-          <p className="font-semibold text-white md:w-[346px] lg:w-[370px]">
-            Temporibus autem quibusdam et aut officiis debitis aut rerum
-            necessitatibus saepe eveniet ut et voluptates repudiandae sint et
-            molestiae non recusandae.
-          </p>
-
-          <button className="rounded-full bg-[#FFC300] p-2 px-16 font-bold text-white border border-[#FFC300] hover:bg-transparent hover:text-[#FFC300] transition-all duration-500">
-            Explore
-          </button>
+          <FadeIn delay={0.2} direction="right">
+            <h1 className="font-poppins font-bold text-[56px] leading-[64px] lg:text-[72px] lg:leading-[72px] lg:w-[335px] w-[261px] text-white">
+              Omnis Voluptas
+            </h1>
+          </FadeIn>
+          <FadeIn delay={0.4} direction="right">
+            <p className="font-semibold text-white md:w-[346px] lg:w-[370px]">
+              Temporibus autem quibusdam et aut officiis debitis aut rerum
+              necessitatibus saepe eveniet ut et voluptates repudiandae sint et
+              molestiae non recusandae.
+            </p>
+          </FadeIn>
+          <FadeIn delay={0.6} direction="right">
+            <button className="rounded-full bg-[#FFC300] p-2 px-16 font-bold text-white border border-[#FFC300] hover:bg-transparent hover:text-[#FFC300] transition-all duration-500">
+              Explore
+            </button>
+          </FadeIn>
         </div>
         {/* illustration */}
 
         <div className="z-10 relative">
-          <Image
-            src={illustration}
-            alt="illustration"
-            className="lg:w-[705px] lg:h-[791px]"
-          />
+          <FadeIn delay={0.2} direction="left">
+            <Image
+              src={illustration}
+              alt="illustration"
+              className="lg:w-[705px] lg:h-[791px]"
+            />
+          </FadeIn>
 
           <div className="rounded-full h-[59px] w-[59px] lg:h-[100px] lg:w-[100px] bg-[linear-gradient(45deg,_#C86DD7_0%,_#3023AE_100%)] absolute lg:-left-[50px] lg:bottom-40 -left-[30px] md:bottom-56 bottom-20 -z-10"></div>
 
--- components/HomeComponent.tsx
+++ components/HomeComponent.tsx
@@ -6,11 +6,31 @@ import homeImage from "@/public/assets/homeImage.svg";
 import homeImage2 from "@/public/assets/homeimage.svg";
 import Image from "next/image";
 import { RxTriangleRight } from "react-icons/rx";
+import FadeIn from "./FadeIn";
+import { motion, Variants } from "framer-motion";
 
 const HomeComponent: FC = () => {
   const [p1, setp1] = useState(false);
   const [p2, setp2] = useState(false);
   const [p3, setp3] = useState(false);
+
+  const pVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      y: -20,
+      transition: {
+        duration: 0.5,
+      },
+    },
+
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8,
+      },
+    },
+  };
   return (
     <div
       className="bg-white p-4 py-12 lg:pt-16 lg:p-12 xl:p-36 flex flex-col lg:flex-row lg:items-center justify-between mx-auto gap-20 lg:gap-0 overflow-hidden"
@@ -18,90 +38,137 @@ const HomeComponent: FC = () => {
     >
       {/* writing */}
       <div className="flex flex-col items-start gap-5">
-        <h5 className="uppercase font-bold font-poppins text-fontBlack">
-          Quis Autem
-        </h5>
-        <h2 className="font-poppins font-bold lg:text-[56px] lg:leading-[64px] lg:w-[498px] sm:w-[356px] text-[40px] leading-[48px]">
-          Corporis suscipit laboriosam
-        </h2>
+        <FadeIn delay={0.2} direction="right">
+          <h5 className="uppercase font-bold font-poppins text-fontBlack">
+            Quis Autem
+          </h5>
+        </FadeIn>
+
+        <FadeIn delay={0.4} direction="right">
+          <h2 className="font-poppins font-bold lg:text-[56px] lg:leading-[64px] lg:w-[498px] sm:w-[356px] text-[40px] leading-[48px]">
+            Corporis suscipit laboriosam
+          </h2>
+        </FadeIn>
 
         {/* paragraph 1 */}
-        <div className="flex flex-col items-start gap-1">
-          <div
-            className="flex items-center gap-1 cursor-pointer"
-            onClick={() => setp1(!p1)}
-          >
-            <p className="font-semibold text-fontBlack">Totam rem aperiam</p>
-            {p1 ? (
-              <IoChevronUpSharp className="text-fontBlack text-lg" />
-            ) : (
-              <IoChevronDownSharp className="text-fontBlack text-lg" />
+        <FadeIn delay={0.6} direction="right">
+          <div className="flex flex-col items-start gap-1">
+            <motion.div
+              className="flex items-center gap-1 cursor-pointer"
+              onClick={() => setp1(!p1)}
+              whileTap={{
+                scale: 0.9,
+              }}
+              whileHover={{
+                scale: 1.05,
+              }}
+            >
+              <p className="font-semibold text-fontBlack">Totam rem aperiam</p>
+              {p1 ? (
+                <IoChevronUpSharp className="text-fontBlack text-lg" />
+              ) : (
+                <IoChevronDownSharp className="text-fontBlack text-lg" />
+              )}
+            </motion.div>
+
+            {p1 && (
+              <motion.p
+                className="text-fontBlack/50 sm:w-[470px] text-justify"
+                variants={pVariants}
+                initial="hidden"
+                animate="visible"
+              >
+                Nam libero tempore, cum soluta nobis est eligendi optio cumque
+                nihil impedit quo minus id quod maxime placeat facere possimus,
+                omnis voluptas assumenda est, omnis dolor repellendus.
+              </motion.p>
             )}
           </div>
-
-          {p1 && (
-            <p className="text-fontBlack/50 w-[470px]">
-              Nam libero tempore, cum soluta nobis est eligendi optio cumque
-              nihil impedit quo minus id quod maxime placeat facere possimus,
-              omnis voluptas assumenda est, omnis dolor repellendus.
-            </p>
-          )}
-        </div>
+        </FadeIn>
 
         {/* paragraph 2 */}
-        <div className="flex flex-col items-start gap-1">
-          <div
-            className="flex items-center gap-1 cursor-pointer"
-            onClick={() => setp2(!p2)}
-          >
-            <p className="font-semibold text-fontBlack">Eaque ipsa quae</p>
-            {p2 ? (
-              <IoChevronUpSharp className="text-fontBlack text-lg" />
-            ) : (
-              <IoChevronDownSharp className="text-fontBlack text-lg" />
+        <FadeIn delay={0.8} direction="right">
+          <div className="flex flex-col items-start gap-1">
+            <motion.div
+              className="flex items-center gap-1 cursor-pointer"
+              onClick={() => setp2(!p2)}
+              whileTap={{
+                scale: 0.9,
+              }}
+              whileHover={{
+                scale: 1.05,
+              }}
+            >
+              <p className="font-semibold text-fontBlack">Eaque ipsa quae</p>
+              {p2 ? (
+                <IoChevronUpSharp className="text-fontBlack text-lg" />
+              ) : (
+                <IoChevronDownSharp className="text-fontBlack text-lg" />
+              )}
+            </motion.div>
+
+            {p2 && (
+              <motion.p
+                className="text-fontBlack/50 sm:w-[470px] text-justify"
+                variants={pVariants}
+                initial="hidden"
+                animate="visible"
+              >
+                Nam libero tempore, cum soluta nobis est eligendi optio cumque
+                nihil impedit quo minus id quod maxime placeat facere possimus,
+                omnis voluptas assumenda est, omnis dolor repellendus.
+              </motion.p>
             )}
           </div>
-
-          {p2 && (
-            <p className="text-fontBlack/50 w-[470px]">
-              Nam libero tempore, cum soluta nobis est eligendi optio cumque
-              nihil impedit quo minus id quod maxime placeat facere possimus,
-              omnis voluptas assumenda est, omnis dolor repellendus.
-            </p>
-          )}
-        </div>
+        </FadeIn>
 
         {/* paragraph 3 */}
-        <div className="flex flex-col items-start gap-1">
-          <div
-            className="flex items-center gap-1 cursor-pointer"
-            onClick={() => setp3(!p3)}
-          >
-            <p className="font-semibold text-fontBlack">Inventore veritatis</p>
-            {p3 ? (
-              <IoChevronUpSharp className="text-fontBlack text-lg" />
-            ) : (
-              <IoChevronDownSharp className="text-fontBlack text-lg" />
+        <FadeIn delay={1.0} direction="right">
+          <div className="flex flex-col items-start gap-1">
+            <motion.div
+              className="flex items-center gap-1 cursor-pointer"
+              onClick={() => setp3(!p3)}
+              whileTap={{
+                scale: 0.9,
+              }}
+              whileHover={{
+                scale: 1.05,
+              }}
+            >
+              <p className="font-semibold text-fontBlack">
+                Inventore veritatis
+              </p>
+              {p3 ? (
+                <IoChevronUpSharp className="text-fontBlack text-lg" />
+              ) : (
+                <IoChevronDownSharp className="text-fontBlack text-lg" />
+              )}
+            </motion.div>
+
+            {p3 && (
+              <motion.p
+                className="text-fontBlack/50 sm:w-[470px] text-justify"
+                variants={pVariants}
+                initial="hidden"
+                animate="visible"
+              >
+                Nam libero tempore, cum soluta nobis est eligendi optio cumque
+                nihil impedit quo minus id quod maxime placeat facere possimus,
+                omnis voluptas assumenda est, omnis dolor repellendus.
+              </motion.p>
             )}
           </div>
-
-          {p3 && (
-            <p className="text-fontBlack/50 w-[470px]">
-              Nam libero tempore, cum soluta nobis est eligendi optio cumque
-              nihil impedit quo minus id quod maxime placeat facere possimus,
-              omnis voluptas assumenda est, omnis dolor repellendus.
-            </p>
-          )}
-        </div>
+        </FadeIn>
       </div>
 
       {/*video  */}
-      <div className="relative">
-        <Image src={homeImage2} alt="home image" className="w-full" />
-        <div className="bg-white shadow-lg lg:h-[116px] lg:w-[116px] h-[58px] w-[58px] rounded-full flex items-center justify-center absolute lg:-left-12 top-1/2 -translate-x-1/2 lg:translate-x-0 -translate-y-1/2 left-1/2">
-          <RxTriangleRight className="lg:text-5xl text-3xl text-secondary" />
-        </div>
-        {/* <div className="w-[355px] h-[272px] sm:w-[592px] sm:h-[470px] md:w-[650px] md:h-[510px] lg:w-[705px] lg:h-[544px] bg-[linear-gradient(45deg,_#C86DD7_0%,_#3023AE_100%)] relative">
+      <FadeIn delay={0.2} direction="left">
+        <div className="relative">
+          <Image src={homeImage2} alt="home image" className="w-full" />
+          <div className="bg-white shadow-lg lg:h-[116px] lg:w-[116px] h-[58px] w-[58px] rounded-full flex items-center justify-center absolute lg:-left-12 top-1/2 -translate-x-1/2 lg:translate-x-0 -translate-y-1/2 left-1/2">
+            <RxTriangleRight className="lg:text-5xl text-3xl text-secondary" />
+          </div>
+          {/* <div className="w-[355px] h-[272px] sm:w-[592px] sm:h-[470px] md:w-[650px] md:h-[510px] lg:w-[705px] lg:h-[544px] bg-[linear-gradient(45deg,_#C86DD7_0%,_#3023AE_100%)] relative">
           <Image
             src={homeImage}
             alt="video"
@@ -109,7 +176,8 @@ const HomeComponent: FC = () => {
           />
         
         </div> */}
-      </div>
+        </div>
+      </FadeIn>
     </div>
   );
 };
