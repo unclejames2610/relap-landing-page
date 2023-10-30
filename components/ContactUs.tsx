924fc59ed8a9b3eeb0ec195116f88a4c575c549a
unclejames2610 <udealaebuka@gmail.com>
2023-10-30 15:18:53 +0100

    feat:animated the contact and people page

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import FadeIn from "./FadeIn";
 import Footer from "./Footer";
 
 const ContactUs: FC = () => {
@@ -8,24 +9,31 @@ const ContactUs: FC = () => {
       id="contact-us"
     >
       <div className="flex items-center gap-8 flex-col text-fontBlack">
-        <h2 className="text-fontBlack font-poppins font-bold lg:text-[56px] lg:leading-[64px] text-[40px] leading-[48px]">
-          Contact Us
-        </h2>
-        <p className="text-center font-semibold w-[280px] sm:w-[346px] md:w-[570px]">
-          Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-          quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-          voluptas.
-        </p>
+        <FadeIn delay={0.4} direction="down">
+          <h2 className="text-fontBlack font-poppins font-bold lg:text-[56px] lg:leading-[64px] text-[40px] leading-[48px]">
+            Contact Us
+          </h2>
+        </FadeIn>
 
-        <div className="flex items-center flex-col lg:flex-row gap-5 w-full justify-center">
-          <input
-            className="border p-4 bg-white border-[#DADADA] lg:w-[60%] w-full focus:outline-none focus:border-secondary outline-none"
-            placeholder="Your email"
-          />
-          <button className="bg-secondary py-3 px-16 rounded-full text-white font-bold border border-secondary hover:bg-transparent hover:text-secondary duration-500 transition-all">
-            Send
-          </button>
-        </div>
+        <FadeIn delay={0.2} direction="down">
+          <p className="text-center font-semibold w-[280px] sm:w-[346px] md:w-[570px]">
+            Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+            quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat
+            quo voluptas.
+          </p>
+        </FadeIn>
+        <FadeIn delay={0.6} direction="up" fullWidth>
+          <div className="flex items-center flex-col lg:flex-row gap-5 w-full justify-center">
+            <input
+              className="border p-4 bg-white border-[#DADADA] lg:w-[60%] w-full focus:outline-none focus:border-secondary outline-none"
+              placeholder="Your email"
+            />
+
+            <button className="bg-secondary py-3 px-16 rounded-full text-white font-bold border border-secondary hover:bg-transparent hover:text-secondary duration-500 transition-all">
+              Send
+            </button>
+          </div>
+        </FadeIn>
       </div>
 
       <div className="h-[1px] w-screen bg-black/10 mt-20 lg:mt-48"></div>
--- components/People.tsx
+++ components/People.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-import React, { FC } from "react";
+import React, { FC, useEffect, useRef } from "react";
 import icon1 from "@/public/assets/peopleIcon1.svg";
 import icon2 from "@/public/assets/peopleIcon2.svg";
 import icon3 from "@/public/assets/peopleIcon3.svg";
@@ -8,8 +9,37 @@ import icon5 from "@/public/assets/peopleIcon5.svg";
 import icon6 from "@/public/assets/peopleIcon6.svg";
 import PeopleCards from "./PeopleCards";
 import FadeIn from "./FadeIn";
+import { motion, useAnimation, useInView, Variants } from "framer-motion";
 
 const People: FC = () => {
+  const ref = useRef(null);
+
+  const isInView = useInView(ref, { once: true });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start("visible");
+    }
+  }, [isInView, controls]);
+
+  const iconVariant: Variants = {
+    hidden: {
+      y: 100,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1.25,
+        type: "tween",
+        delay: 1.5,
+        ease: [0.25, 0.25, 0.25, 0.75],
+      },
+    },
+  };
+
   return (
     <div
       className="flex flex-col p-4 py-12 lg:pt-16 lg:p-12 xl:p-36 h-full w-full gap-8 lg:gap-16"
@@ -38,16 +68,21 @@ const People: FC = () => {
       </div>
 
       {/* icons */}
-      <FadeIn delay={0.8} direction="up">
-        <div className="flex flex-wrap justify-between items-center gap-8 mt-12 opacity-50">
+      <div ref={ref}>
+        <motion.div
+          className="flex flex-wrap justify-between items-center gap-8 mt-12 opacity-50"
+          variants={iconVariant}
+          initial="hidden"
+          animate={controls}
+        >
           <Image src={icon1} alt="icon 1" />
           <Image src={icon2} alt="icon 2" />
           <Image src={icon3} alt="icon 3" />
           <Image src={icon4} alt="icon 4" />
           <Image src={icon5} alt="icon 5" />
           <Image src={icon6} alt="icon 6" />
-        </div>
-      </FadeIn>
+        </motion.div>
+      </div>
     </div>
   );
 };
